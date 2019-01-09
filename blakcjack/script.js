9ae3cb1fae21aee853f8a5ed6084df922fcb9eb1
IlyaOrluk <misfits120@mail.ru>
2019-01-10 02:46:57 +0300

    beta2.0

--- blakcjack/script.js
+++ blakcjack/script.js
@@ -1,27 +1,22 @@
 let box = document.querySelector('.card'),
     playerScore = document.querySelector('.player_score'),
     compScore = document.querySelector('.comp_score');
-let down = 6, move = 50, rotate = 0;
-console.log(playerScore);
-console.log(compScore);
+let down = 6, move = 42, rotate = 0;
 
 box.style.left = `${move}%`;
 box.style.top = `${down}%`;
 
-
-    
-
-
 let more = document.querySelector('.more'),
-    open = document.querySelector('.open');
-let arr = [6,7,8,9,10,2,3,4,11],
+    open = document.querySelector('.open'),
+    arr = [6,7,8,9,10,2,3,4,11],
     
     players = {
         one: 0,
         two: 0,
         playerCards: 0,
         compCards: 0,
-        compOpen: true
+        compOpen: true,
+        suitId: [1,2,3,4]
     },
 
     restart = () => {
@@ -41,107 +36,129 @@ let arr = [6,7,8,9,10,2,3,4,11],
     card.classList.add('card');
     document.body.appendChild(card);
 
-    card.style.left = `50%`;
+    card.style.left = `42%`;
     card.style.top = `6%`;
 
     front.classList.add('front');
     card.appendChild(front);
+    },
+    cardName = (back, randomCard, randomSuit) =>{
+        let suit = document.createElement('img'),
+            suitReverse = document.createElement('img'),
+            suitCenter = document.createElement('img'),
+            nameCard = document.createElement('p'),
+            nameCardReverse = document.createElement('p');
 
-    2
-};
+            suit.classList.add('suit');
+            suitReverse.classList.add('suit_reverse');
+            suitCenter.classList.add('suit_center');
+            nameCard.classList.add('name_card');
+            nameCardReverse.classList.add('name_card_reverse');
+
+        let cardSuit = () => {
+            randomSuit = Math.floor( Math.random() * 4 ) + 1;
+            suit.src = `${randomSuit}.png`;
+            suitReverse.src = `${randomSuit}.png`;
+            suitCenter.src = `${randomSuit}.png`;
+            back.appendChild(suit);
+            back.appendChild(suitReverse);
+            back.appendChild(suitCenter);
+            back.appendChild(nameCard);
+            back.appendChild(nameCardReverse);
+        }; 
+
+        if (randomCard === 5) {
+            nameCard.innerText = 'J';
+            nameCardReverse.innerText = 'J';
+            cardSuit();
+        } else if (randomCard === 6) {
+            nameCard.innerText = 'Q';
+            nameCardReverse.innerText = 'Q';
+            cardSuit();
+        } else if (randomCard === 7) {
+            nameCard.innerText = 'K';
+            nameCardReverse.innerText = 'K';
+            cardSuit();
+        } else if (randomCard === 8) {
+            nameCard.innerText = 'A';
+            nameCardReverse.innerText = 'A';
+            cardSuit();
+        } else {
+            nameCard.innerText = arr[randomCard];
+            nameCardReverse.innerText = arr[randomCard];
+            cardSuit();
+        }
+
+    }
+
+    
 
 //Comp
 let CompCard = document.createElement('div');
-let CompFront = document.createElement('span');
-let CompBack = document.createElement('span');
-let openCompCard = () => {
-    CompCard = document.createElement('div');
-    CompFront = document.createElement('span');
-    CompBack = document.createElement('span');
-    newCard(CompCard, CompFront);
-    
-    CompBack.classList.add('back');
-    CompCard.appendChild(CompBack);
-    CompBack.innerText = arr[CompRandomCard];
-
-    if (CompRandomCard === 5) {
-        CompBack.innerText = 'J';
-    } else if (CompRandomCard === 6) {
-        CompBack.innerText = 'Q';
-    } else if (CompRandomCard === 7) {
-        CompBack.innerText = 'K';
-    } else if (CompRandomCard === 8) {
-        CompBack.innerText = 'A';
-    } else {
-        CompBack.innerText = arr[CompRandomCard];
-    }
-    
-    
-    
-    
-    
-    let rotateCard = () =>{
-
-        move = 60;
-        down = 50;
-    
-        players.compCards += 1;
-        for(let i = 1; i < players.compCards; i++){
-            move += 1.5;
+    CompFront = document.createElement('span'),
+    CompBack = document.createElement('span'),
+    CompRandomSuit = Math.floor( Math.random() * 4 ) + 1,
+    openCompCard = () => {
+        CompCard = document.createElement('div');
+        CompFront = document.createElement('span');
+        CompBack = document.createElement('span');
+        newCard(CompCard, CompFront);
+        
+        CompBack.classList.add('back');
+        CompCard.appendChild(CompBack);
+        cardName(CompBack,CompRandomCard, CompRandomSuit);
+        
+        let rotateCard = () =>{
+            move = 60;
+            down = 50;
+        
+            players.compCards += 1;
+            for(let i = 1; i < players.compCards; i++){
+                move += 1.5;
+            }
+        if (players.compOpen == true){
+                CompFront.style.transform = `rotateX(180deg)`;
+                CompBack.style.transform = `rotateX(360deg)`;
+                // players.compOpen = false;
         }
-       if (players.compOpen == true){
-            CompFront.style.transform = `rotateX(180deg)`;
-            CompBack.style.transform = `rotateX(360deg)`;
-            // players.compOpen = false;
-       }
         CompCard.style.left = `${move}%`;
         CompCard.style.top = `${down}%`;
         CompCard.style.transform = `rotateZ(360deg)`;
 
 
-    };
-    setTimeout(rotateCard, 300);
+        };
+        setTimeout(rotateCard, 300);
 };
     
 
 //Player
 let playerCard = document.createElement('div');
-let playerFront = document.createElement('span');
-let playerBack = document.createElement('span');
-let openPlayerCard = () => {
-    playerCard = document.createElement('div');
-    playerFront = document.createElement('span');
-    playerBack = document.createElement('span');
-    newCard(playerCard, playerFront);
-    playerBack.classList.add('back');
-    playerCard.appendChild(playerBack);
-
-    if (playerRandomCard == 5) {
-        playerBack.innerText = 'J';
-    } else if (playerRandomCard == 6) {
-        playerBack.innerText = 'Q';
-    } else if (playerRandomCard == 7) {
-        playerBack.innerText = 'K';
-    } else if (playerRandomCard == 8) {
-        playerBack.innerText = 'A';
-    } else {
-        playerBack.innerText = arr[playerRandomCard];
-    }
-
-    let rotateCard = () =>{
+    playerFront = document.createElement('span'),
+    playerBack = document.createElement('span'),
+    playerRandomSuit = Math.floor( Math.random() * 4 ) + 1,
+    suit = document.createElement('img'),
+    openPlayerCard = () => {
+        playerCard = document.createElement('div');
+        playerFront = document.createElement('span');
+        playerBack = document.createElement('span');
+        newCard(playerCard, playerFront);
+        playerBack.classList.add('back');
+        playerCard.appendChild(playerBack);
+        cardName(playerBack, playerRandomCard, playerRandomSuit);
         
-        move = 20;
-        down = 50;
-    
-        players.playerCards += 1;
-        for(let i = 1; i < players.playerCards; i++){
-            move += 1.5;
-        }
-        playerCard.style.left = `${move}%`;
-        playerCard.style.top = `${down}%`;
-        playerFront.style.transform = `rotateX(180deg)`;
-        playerBack.style.transform = `rotateX(360deg)`;
-        playerCard.style.transform = `rotateZ(360deg)`;
+        let rotateCard = () =>{
+            move = 20;
+            down = 50;
+        
+            players.playerCards += 1;
+            for(let i = 1; i < players.playerCards; i++){
+                move += 1.5;
+            }
+            playerCard.style.left = `${move}%`;
+            playerCard.style.top = `${down}%`;
+            playerFront.style.transform = `rotateX(180deg)`;
+            playerBack.style.transform = `rotateX(360deg)`;
+            playerCard.style.transform = `rotateZ(360deg)`;
 
     };
     setTimeout(rotateCard, 300);
@@ -160,7 +177,6 @@ more.addEventListener('click', () => {
     playerScore.innerText = `Your score: ${players.one}`;
 
     setTimeout(openPlayerCard, 100);
-    
     
     if (players.two <= 17) {
         players.two += arr[CompRandomCard];
