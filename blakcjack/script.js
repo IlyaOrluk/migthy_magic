71d4403d87dda00662a093f6e4db3f96d2aa6cff
IlyaOrluk <misfits120@mail.ru>
2019-01-18 00:46:56 +0300

    blackJack 0.1

--- blakcjack/script.js
+++ blakcjack/script.js
@@ -1,24 +1,27 @@
-let box = document.querySelector('.card'),
-    playerScore = document.querySelector('.player_score'),
+let playerScore = document.querySelector('.player_score'),
     compScore = document.querySelector('.comp_score'),
-    down = 6, move = 42, rotate = 0;
-
-box.style.left = `${move}%`;
-box.style.top = `${down}%`;
-
-let more = document.querySelector('.more'),
+    down = 6, move = 42, rotate = 0,
+    more = document.querySelector('.more'),
     open = document.querySelector('.open'),
-    arr = [6,7,8,9,10,2,3,4,11],
-    
-    players = {
+    data = {
+        cardList: [],
         one: 0,
         two: 0,
         playerCards: 0,
         compCards: 0,
-        compOpen: true,
-        suitId: [1,2,3,4],
-        repeatCard: []
+        compOpen: true
     },
+    playerRandomCard = Math.floor( Math.random() * data.cardList.length ),
+    CompRandomCard = Math.floor( Math.random() * data.cardList.length ),
+    cardsList = () => {
+        data.cardList = [
+            [6,1],[7,1],[8,1],[9,1],[10,1],[2,1],[3,1],[4,1],[11,1], //hearts <3
+            [6,2],[7,2],[8,2],[9,2],[10,2],[2,2],[3,2],[4,2],[11,2], //peaks <3<
+            [6,3],[7,3],[8,3],[9,3],[10,3],[2,3],[3,3],[4,3],[11,3], //baptize +
+            [6,4],[7,4],[8,4],[9,4],[10,4],[2,4],[3,4],[4,4],[11,4] //diamonds <>
+        ];
+    };
+    cardsList();
 
     restart = () => {
     let cards = document.querySelectorAll('.card');
@@ -26,11 +29,12 @@ let more = document.querySelector('.more'),
                 document.body.removeChild(item);
             });
             playerScore.innerText = `Your score: 0`;
-            players.one = 0;
-            players.two = 0;
-            players.compCards = 0;
-            players.playerCards = 0;
-            players.repeatCard = [];
+            compScore.innerText = `Comp score: 0`;
+            data.one = 0;
+            data.two = 0;
+            data.compCards = 0;
+            data.playerCards = 0;
+            cardsList();
 
     },
     
@@ -44,13 +48,21 @@ let more = document.querySelector('.more'),
     front.classList.add('front');
     card.appendChild(front);
     },
-    cardName = (back, randomCard, randomSuit) =>{
+    playerPlusScore = (randomCard) => {
+        data.one += data.cardList[randomCard][0];
+        playerScore.innerText = `Your score: ${data.one}`;
+    },
+    CompPlusScore = (randomCard) => {
+        data.two += data.cardList[randomCard][0];
+        compScore.innerText = `Comp score: ${data.two}`;
+    },
+    cardName = (back, randomCard) =>{
         let suit = document.createElement('img'),
             suitReverse = document.createElement('img'),
             suitCenter = document.createElement('img'),
             nameCard = document.createElement('p'),
             nameCardReverse = document.createElement('p');
-
+            
             suit.classList.add('suit');
             suitReverse.classList.add('suit_reverse');
             suitCenter.classList.add('suit_center');
@@ -58,9 +70,9 @@ let more = document.querySelector('.more'),
             nameCardReverse.classList.add('name_card_reverse');
 
         let cardSuit = () => {
-            suit.src = `${randomSuit}.png`;
-            suitReverse.src = `${randomSuit}.png`;
-            suitCenter.src = `${randomSuit}.png`;
+            suit.src = `${data.cardList[randomCard][1]}.png`;
+            suitReverse.src = `${data.cardList[randomCard][1]}.png`;
+            suitCenter.src = `${data.cardList[randomCard][1]}.png`;
             back.appendChild(suit);
             back.appendChild(suitReverse);
             back.appendChild(suitCenter);
@@ -68,140 +80,123 @@ let more = document.querySelector('.more'),
             back.appendChild(nameCardReverse);
         }; 
 
-        if (randomCard === 5) {
+        if (data.cardList[randomCard][0] == 2) {
             nameCard.innerText = 'J';
             nameCardReverse.innerText = 'J';
-            players.repeatCard.push(`J-${randomSuit}`);
             cardSuit();
-            
-        } else if (randomCard === 6) {
+        } else if (data.cardList[randomCard][0] == 3) {
             nameCard.innerText = 'Q';
             nameCardReverse.innerText = 'Q';
-            players.repeatCard.push(`Q-${randomSuit}`);
             cardSuit();
-        } else if (randomCard === 7) {
+        } else if (data.cardList[randomCard][0] == 4) {
             nameCard.innerText = 'K';
             nameCardReverse.innerText = 'K';
-            players.repeatCard.push(`K-${randomSuit}`);
             cardSuit();
-        } else if (randomCard === 8) {
+        } else if (data.cardList[randomCard][0] == 11) {
             nameCard.innerText = 'A';
             nameCardReverse.innerText = 'A';
-            players.repeatCard.push(`A-${randomSuit}`);
             cardSuit();
         } else {
-            nameCard.innerText = arr[randomCard];
-            nameCardReverse.innerText = arr[randomCard];
-            players.repeatCard.push(`${randomCard}-${randomSuit}`);
+            nameCard.innerText = data.cardList[randomCard][0];
+            nameCardReverse.innerText = data.cardList[randomCard][0];
             cardSuit();
         }
-        console.log(players.repeatCard);
+        data.cardList.splice(randomCard, 1);
+        console.log(data.cardList);
+        console.log(data.one);
+        console.log(data.two);
     },
     getWinner = () => {
-        if(players.two > 21 && players.one > 21){
+        if(data.two > 21 && data.one > 21){
             setTimeout(alert('DRAW!=)'), 1000);
             setTimeout(restart, 1000);
-        } else if(players.two == 21 && players.one == 21){
+        } else if(data.two == 21 && data.one == 21){
             setTimeout(alert('DRAW!=)'), 1000);
             setTimeout(restart, 1000);
-        } else if(players.one > 21){
-            setTimeout(alert('You lose, Comp score: ' + players.two), 1000);
+        } else if(data.one > 21){
+            setTimeout(alert('You lose, Comp score: ' + data.two), 1000);
             setTimeout(restart, 1000);
-        } else if(players.two > 21){
-            setTimeout(alert('You WIN!, Comp score: ' + players.two), 1000);
+        } else if(data.two > 21){
+            setTimeout(alert('You WIN!, Comp score: ' + data.two), 1000);
             setTimeout(restart, 1000);
-        } else if(players.one == 21) {
-            setTimeout(alert('21! You WIN!, Comp score: ' + players.two), 1000);
+        } else if(data.one == 21) {
+            setTimeout(alert('21! You WIN!, Comp score: ' + data.two), 1000);
             setTimeout(restart, 1000);
-        } else if(players.two == 21) {
-            setTimeout(alert('Comp WIN!, Comp score: ' + players.two + '!'), 1000);
+        } else if(data.two == 21) {
+            setTimeout(alert('Comp WIN!, Comp score: ' + data.two + '!'), 1000);
             setTimeout(restart, 1000);
-        } else if(players.two > players.one){
-            setTimeout(alert('Comp WIN!, Comp score: ' + players.two), 1000);
+        } else if(data.two > data.one){
+            setTimeout(alert('Comp WIN!, Comp score: ' + data.two), 1000);
             setTimeout(restart, 1000);
-        } else if(players.two < players.one){
-            setTimeout(alert('You WIN!, Comp score: ' + players.two), 1000);
+        } else if(data.two < data.one){
+            setTimeout(alert('You WIN!, Comp score: ' + data.two), 1000);
             setTimeout(restart, 1000);
-        }  else if(players.two == players.one){
+        }  else if(data.two == data.one){
             setTimeout(alert('DRAW! :)'), 1000);
             setTimeout(restart, 1000);
         }
     },
-    
     openComp = () => {
-                players.compOpen = true;
-                CompRandomCard = Math.floor( Math.random() * 9 );
-                openCompCard();
-                players.two += arr[CompRandomCard];
-                if (players.two > 18) {
+            data.compOpen = true;
+            openCompCard();
+                if (data.two > 18) {
                     clearInterval(autoOpen);
                     setTimeout(getWinner, 1000);
                 }
     };
 
-    
-
 //Comp
-let CompCard = document.createElement('div');
-    CompFront = document.createElement('span'),
-    CompBack = document.createElement('span'),
-    CompRandomSuit = Math.floor( Math.random() * 4 ) + 1,
     openCompCard = () => {
-        CompCard = document.createElement('div');
+    let CompCard = document.createElement('div');
         CompFront = document.createElement('span');
         CompBack = document.createElement('span');
-        CompRandomSuit = Math.floor( Math.random() * 4 ) + 1;
         newCard(CompCard, CompFront);
         
         CompBack.classList.add('back');
         CompCard.appendChild(CompBack);
-        cardName(CompBack,CompRandomCard, CompRandomSuit);
+        CompRandomCard = Math.floor( Math.random() * data.cardList.length );
+        cardName(CompBack ,CompRandomCard, CompPlusScore(CompRandomCard));
         
         let rotateCard = () =>{
             move = 60;
             down = 50;
         
-            players.compCards += 1;
-            for(let i = 1; i < players.compCards; i++){
+            data.compCards += 1;
+            for(let i = 1; i < data.compCards; i++){
                 move += 1.5;
             }
-        if (players.compOpen == true){
+        if (data.compOpen == true){
                 CompFront.style.transform = `rotateX(180deg)`;
                 CompBack.style.transform = `rotateX(360deg)`;
-                // players.compOpen = false;
+                // data.compOpen = false;
         }
         CompCard.style.left = `${move}%`;
         CompCard.style.top = `${down}%`;
         CompCard.style.transform = `rotateZ(360deg)`;
 
 
-        };
-        setTimeout(rotateCard, 300);
+    };
+    setTimeout(rotateCard, 300);
 };
     
-
 //Player
-let playerCard = document.createElement('div');
-    playerFront = document.createElement('span'),
-    playerBack = document.createElement('span'),
-    playerRandomSuit = Math.floor( Math.random() * 4 ) + 1,
-    suit = document.createElement('img'),
     openPlayerCard = () => {
-        playerCard = document.createElement('div');
+    let playerCard = document.createElement('div');
         playerFront = document.createElement('span');
         playerBack = document.createElement('span');
-        playerRandomSuit = Math.floor( Math.random() * 4 ) + 1;
         newCard(playerCard, playerFront);
+
         playerBack.classList.add('back');
         playerCard.appendChild(playerBack);
-        cardName(playerBack, playerRandomCard, playerRandomSuit);
+        playerRandomCard = Math.floor( Math.random() * data.cardList.length );
+        cardName(playerBack, playerRandomCard, playerPlusScore(playerRandomCard));
         
         let rotateCard = () =>{
             move = 20;
             down = 50;
         
-            players.playerCards += 1;
-            for(let i = 1; i < players.playerCards; i++){
+            data.playerCards += 1;
+            for(let i = 1; i < data.playerCards; i++){
                 move += 1.5;
             }
             playerCard.style.left = `${move}%`;
@@ -214,45 +209,25 @@ let playerCard = document.createElement('div');
     setTimeout(rotateCard, 300);
 };
 
-
-let CompRandomCard = Math.floor( Math.random() * 9 ),
-    playerRandomCard = Math.floor( Math.random() * 9 );
-
 //MORE
 more.addEventListener('click', () => {
-    CompRandomCard = Math.floor( Math.random() * 9 );
-    playerRandomCard = Math.floor( Math.random() * 9 );
-    players.one += arr[playerRandomCard];
-    playerScore.innerText = `Your score: ${players.one}`;
+    
 
     setTimeout(openPlayerCard, 100);
     
-    if (players.two <= 17) {
-        players.two += arr[CompRandomCard];
+    if (data.two <= 17) {
         setTimeout(openCompCard, 300);
     } else {
         return '';
     }
-    console.log(players.repeatCard);
 });
-
-//Open
+//OPEN
 let autoOpen = null;
 open.addEventListener('click', () => { 
-    if (players.two < 18){
+    if (data.two < 18){
     autoOpen = setInterval(openComp, 1000);
     } else {
         setTimeout(getWinner, 1000);
-    }
-        
+    }   
 });
 
-
-// var peopleList = ['dodo', 'djorhe', 'genry'];
-// let changePerson = () => {
-//   let randomPerson = Math.floor(Math.random() * peopleList.length);
-//   alert(randomPerson);
-//   peopleList.splice(randomPerson, 1);
-//   alert(peopleList);
-// };
-// changePerson();
